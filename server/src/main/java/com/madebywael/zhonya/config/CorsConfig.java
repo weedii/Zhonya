d9f0fb47e5dea661d6e0f97a88d91e7dc1a7471b
weedii <abidiw293@gmail.com>
2024-12-27 22:14:41 +0100

    fix cors

--- server/src/main/java/com/madebywael/zhonya/config/CorsConfig.java
+++ server/src/main/java/com/madebywael/zhonya/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.madebywael.zhonya.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,6 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${application.frontend.url}")
+    private String frontendUrl;
+
     @Bean
     public WebMvcConfigurer webMvcConfigurer() {
         return new WebMvcConfigurer() {
@@ -15,7 +19,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PATCH", "DELETE")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(frontendUrl)
                         .allowCredentials(true);
             }
         };
